204069c714456f7cbb61086735187d4e5cdbe382
Cameltr <cameltr@whu.edu.cn>
2023-03-07 20:22:30 +0800

    Update FeatureAlignment.py

--- models/FAM/FeatureAlignment.py
+++ models/FAM/FeatureAlignment.py
@@ -1,15 +1,14 @@
 import torch.nn as nn
 import torch
 
-from models.FAM.DeformableBlock import DeformableConvBlock,DeformableConvBlock_show
+from models.FAM.DeformableBlock import DeformableConvBlock
 from util.util import showpatch
 
 class FAM(nn.Module):
     def __init__(self,in_channels):
         super(FAM, self).__init__()
-        self.deformblock = DeformableConvBlock(input_channels= in_channels)
+        self.deformblock = DeformableConvBlock(input_channels = in_channels*2)
         
-        #对齐了两次次
     def forward(self,ist_feature, rst_feature):
 
         st_out = self.deformblock(ist_feature, rst_feature) #输出aligned feature
